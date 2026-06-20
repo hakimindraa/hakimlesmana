7f163d42a76c144fb55be9df7d78b11c3bacb598
hakimindra <githublesmana@gmail.com>
2026-06-20 21:26:05 +0700

    Allow images from i.ibb.co.com

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'i.ibb.co.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'ibb.co',
       },
     ],
